0f02b3429852f294cbfea821859897f4729170dc
Mariorl14 <mariorl040301@gmaiol.com>
2023-03-17 16:04:37 -0600

    agregado el port en db

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: process.env.DB_Host,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
