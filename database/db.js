34f47855eeb11fe7a4383d358b6f8d940ac78b83
Mariorl14 <mariorl040301@gmaiol.com>
2023-03-19 12:12:36 -0600

    sweetalert, db cloud sql, register responsive

--- database/db.js
+++ database/db.js
@@ -1,9 +1,17 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
+    /*
     host     : 'database-2.cmlp3fj6leck.us-east-1.rds.amazonaws.com',
     user     : 'admin',
     password : 'Ganador02',
     database : 'favtel'
+    */
+
+    host     : '34.30.89.2',
+    user     : 'root',
+    password : 'Ganador02',
+    database : 'favtel'
+
 });
 
 connection.connect((error)=>{
