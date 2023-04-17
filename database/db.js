ce1e6c28c361022659abd4625ff6df5fc347b083
Mariorl14 <mariorl040301@gmaiol.com>
2023-04-17 00:11:52 -0600

    prueba

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    /*
-    host     : 'database-2.cmlp3fj6leck.us-east-1.rds.amazonaws.com',
+    
+    host     : 'database-2.cmr2yxsgwbk6.us-east-1.rds.amazonaws.com',
     user     : 'admin',
-    password : 'Ganador02',
-    database : 'favtelbd'
-    */
+    password : '123456789',
+    database : 'favtel'
+    
 
    /*
     host     : '34.30.89.2',
@@ -14,11 +14,12 @@ const connection = mysql.createConnection({
     database : 'favtel'
     */
     
-
+    /*
     host     : 'localhost',
     user     : 'root',
     password : '',
     database : 'login_node'
+    */
 
 });
 
