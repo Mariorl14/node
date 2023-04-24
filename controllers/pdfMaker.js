91428a70c8aed3cd68e312f9feab1028b364fed2
Mariorl14 <mariorl040301@gmaiol.com>
2023-04-24 06:27:00 -0600

    cambios pdf ntr

--- controllers/pdfMaker.js
+++ controllers/pdfMaker.js
@@ -4,84 +4,150 @@ const path = require('path');
 const fs = require('fs');
 
 function generatePayslip(req, res) {
-  const { employeeName, salary, tax, netSalary } = req.body;
+  const { employeeName, horas, totalVentas } = req.body;
 
+  var SalarioBase = 300000;
+
+  var valorHora = 2500;
+
+  var extradeHoras = horas*valorHora;
+
+  var SalarioNeto = SalarioBase+extradeHoras;
+ 
   // Create a new PDF document
-  // Create a document
   const doc = new PdfkitConstruct({
     size: 'A4',
-    margins: {top: 20, left: 10, right: 10, bottom: 20},
+    margins: {top: 30, left: 10, right: 10, bottom: 20},
     bufferPages: true,
-});
+  });
+
+  // Add logo image to the header
+  doc.setDocumentHeader({
+    height: '26',
+    margins: {top: 5, left: -10, right: 10, bottom: 30},
+  }, () => {
+    const imageBuffer = fs.readFileSync('public/img/Favtel_isotipo.png');
+    const logoWidth = 90;
+    const logoHeight = 90;
+    const logoX = (doc.page.width - logoWidth) / 2;
+    const logoY = doc.header.y + 10;
+    doc.image(imageBuffer, logoX, logoY, {width: logoWidth, height: logoHeight});  
+
+    doc.fontSize(13);
+
+    doc.text('Cedula: 118050243', {
+      width: 420,
+      align: 'left'
+    });
+    
+    doc.text('Nombre: Mario Rodriguez', {
+      width: 420,
+      align: 'left'
+    });
+    
+    doc.text('Fecha de Ingreso: 04/04/2023', {
+      width: 420,
+      align: 'left'
+    });
+    
+    doc.text('Fecha de Pago: 23/04/2023', {
+      width: 420,
+      align: 'left'
+    });
+  });
+  
+  // Move down after the header image
+  
+  const products = [
+    {
+      name: employeeName,
+      horasTrabajadas: horas,
+      ventas: totalVentas,
+      salarioNeto: SalarioNeto
+    }
+  ];
 
 
-// set the header to render in every page
-doc.setDocumentHeader({}, () => {
+  /*
+  doc.text("Sales Report", { align: "center", fontSize: 16, bold: true });
+  */
+  doc.addTable(
+    [
+      {key: 'name', label: 'Vendedor', align: 'center'},
+      {key: 'horasTrabajadas', label: 'Horas Trabajadas', align: 'center'},
+      {key: 'ventas', label: 'Total de Ventas', align: 'center'},
+      {key: 'salarioNeto', label: 'Salario Neto', align: 'center'}
+    ],
+    products, {
+      border: null,
+      width: "fill_body",
+      striped: true,
+      stripedColors: ["#5b68c0", "#d6c4dd"],
+      cellsPadding: 10,
+      marginLeft: 45,
+      marginRight: 45,
+      headAlign: 'center'
+    });
+    doc.text("Table Title", {
+      align: "center",
+      fontSize: 14,
+      bold: true,
+      margin: [0, 10]
+   });
 
 
-  doc.lineJoin('miter')
-      .rect(0, 0, doc.page.width, doc.header.options.heightNumber).fill("#ededed");
+    
+    const deducciones = [
+      {
+        name: 'employeeName',
+        price: 'salary',
+        quantity: 'tax',
+        amount: 'netSalary'
+      }
+    ];
+    
+    doc.moveDown(10);
+    doc.addTable(
+      [
+        {key: 'name', label: 'Product', align: 'center'},
+        {key: 'price', label: 'Price', align: 'center'},
+        {key: 'quantity', label: 'Quantity', align: 'center'},
+        {key: 'amount', label: 'Amount', align: 'center'}
+      ],
+      deducciones, {
+        border: null,
+        width: "fill_body",
+        striped: true,
+        stripedColors: ["#f6f6f6", "#d6c4dd"],
+        cellsPadding: 10,
+        marginLeft: 45,
+        marginRight: 45,
+        headAlign: 'center'
+      });
 
-  doc.fill("#115dc8")
-      .fontSize(20)
-      .text("Reporte de Pagos", doc.header.x, doc.header.y);
-});
+  // render tables
+  doc.render();
 
+  // this should be the last
+  // for this to work you need to set bufferPages to true in constructor options 
+  doc.setPageNumbers((p, c) => `Page ${p} of ${c}`, "bottom right");
 
-// set the footer to render in every page
-doc.setDocumentFooter({}, () => {
+  // set the footer to render in every page
+  doc.setDocumentFooter({}, () => {
 
-  doc.lineJoin('miter')
-      .rect(0, doc.footer.y, doc.page.width, doc.footer.options.heightNumber).fill("#c2edbe");
+    doc.lineJoin('miter')
+        .rect(0, doc.footer.y, doc.page.width, doc.footer.options.heightNumber).fill("#c2edbe");
 
-  doc.fill("#7416c8")
-      .fontSize(8)
-      .text("Favtel Costa Rica", doc.footer.x, doc.footer.y + 10);
-});
+    doc.fill("#7416c8")
+        .fontSize(8)
+        .text("Favtel Costa Rica", doc.footer.x, doc.footer.y + 10);
+  });
 
-const products = [
-  {
-    name: 'Mario',
-    price: 100,
-    quantity: 5,
-    amount: 12
-  }
-];
-
-  // add a table (you can add multiple tables with different columns)
-            // make sure every column has a key. keys should be unique
-            doc.addTable(
-              [
-                  {key: 'name', label: 'Product', align: 'left'},
-                  {key: 'price', label: 'Price', align: 'right'},
-                  {key: 'quantity', label: 'Quantity'},
-                  {key: 'amount', label: 'Amount', align: 'right'}
-              ],
-              products, {
-                  border: null,
-                  width: "fill_body",
-                  striped: true,
-                  stripedColors: ["#f6f6f6", "#d6c4dd"],
-                  cellsPadding: 10,
-                  marginLeft: 45,
-                  marginRight: 45,
-                  headAlign: 'center'
-              });
-
-
-          // render tables
-          doc.render();
-
-          // this should be the last
-          // for this to work you need to set bufferPages to true in constructor options 
-          doc.setPageNumbers((p, c) => `Page ${p} of ${c}`, "bottom right");
-
-           // Send the PDF document as a response to the client
+  // Send the PDF document as a response to the client
   res.setHeader('Content-Disposition', 'attachment; filename=payslip.pdf');
   res.contentType('application/pdf');
-          doc.pipe(res);
-          doc.end();
- 
+  doc.pipe(res);
+  doc.end();
 }
 
 module.exports = { generatePayslip };
