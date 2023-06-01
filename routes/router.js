6ab5d837452c49379f5be9da0d4c6a71ccc02a0b
Mariorl14 <mariorl040301@gmail.com>
2023-06-01 16:27:53 -0600

    register

--- routes/router.js
+++ routes/router.js
@@ -94,7 +94,7 @@ router.get('/home', authController.isAuthenticated,authController.authRol, NoCac
     res.render('home', {rows:rows,user:user});
 
 })
-router.get('/register',  (req, res)=>{
+router.get('/register', authController.isAuthenticated,authController.authRol, NoCache.nocache,  async (req, res)=>{
     res.render('register', {user:req.user})
 })
 router.get('/layout',  (req, res)=>{
