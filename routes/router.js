3add5512b6a7df1ad0a66be6fb4b40837755c8d1
Mariorl14 <mariorl040301@gmaiol.com>
2023-06-06 19:52:52 -0600

    edit en home

--- routes/router.js
+++ routes/router.js
@@ -80,7 +80,7 @@ router.get('/home', authController.isAuthenticated,authController.authRol, NoCac
     const ventas = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
-        range: "Respuestas_Formulario!A2:AC",
+        range: "Respuestas_Formulario!A2:Aj",
     })
 
     var user = req.user;
@@ -479,6 +479,7 @@ router.get('/borrarUser/:id', (req, res)=>{
     })
 });
 
+/* editar base CLARO */
 router.get('/edit/:rowId', async  (req, res) => {
 
     const auth = new google.auth.GoogleAuth({
@@ -567,7 +568,7 @@ router.get('/edit/:rowId', async  (req, res) => {
       res.redirect('/bdClaro'); // Redirect to the main page or any other desired location
     });
   });
-
+/*eDITAR BASE KOLBIII */
   router.get('/editKolbi/:rowId', async  (req, res) => {
 
     const auth = new google.auth.GoogleAuth({
@@ -658,6 +659,121 @@ router.get('/edit/:rowId', async  (req, res) => {
     });
   });
 
+/*EDITAR HOMEE */
+  router.get('/editHOME/:rowId', async  (req, res) => {
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+      });
+      
+      // Create the client instance for authentication
+      const client = await auth.getClient();
+      
+      // Create an instance of the Google Sheets API
+      const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    // Get DATA 
+    const rowId = req.params.rowId;
+  
+    // Step 2: Retrieve row data from Google Sheets
+    // Set up authentication as mentioned in the previous response
+  
+    const spreadsheetId = '1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY';
+    const range = `Respuestas_Formulario!A${rowId}:AJ${rowId}`;
+  
+    googleSheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    }, (err, response) => {
+      if (err) {
+        console.error('The API returned an error:', err);
+        res.send('Error retrieving row data');
+        return;
+      }
+  
+      const rowValues = response.data.values[0];
+  
+      // Step 3: Render the update view with retrieved data
+      res.render('editarHOME', { rowId, rowValues }); // Assumes you have an 'update.ejs' view/template
+    });
+  });
+ router.post('/editHOME/:rowId', async (req, res) => {
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+      });
+      
+      // Create the client instance for authentication
+      const client = await auth.getClient();
+      
+      // Create an instance of the Google Sheets API
+      const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    const rowId = req.params.rowId;
+    const updatedValues = [
+      req.body.column1,
+      req.body.column2,
+      req.body.column3,
+      req.body.column4,
+      req.body.column5,
+      req.body.column6,
+      req.body.column7,
+      req.body.column8,
+      req.body.column9,
+      req.body.column10,
+      req.body.column11,
+      req.body.column12,
+      req.body.column13,
+      req.body.column14,
+      req.body.column15,
+      req.body.column16,
+      req.body.column17,
+      req.body.column18,
+      req.body.column19,
+      req.body.column20,
+      req.body.column21,
+      req.body.column22,
+      req.body.column23,
+      req.body.column24,
+      req.body.column25,
+      req.body.column26,
+      req.body.column27,
+      req.body.column28,
+      req.body.column29,
+      req.body.column30,
+      req.body.column31,
+      req.body.column32,
+      req.body.column33,
+      req.body.column34,
+      req.body.column35,
+    ];
+  
+    // Set up authentication as mentioned in the previous response
+  
+    const spreadsheetId = '1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY';
+    const range = `Respuestas_Formulario!A${rowId}:AJ${rowId}`;
+  
+    const requestBody = {
+      values: [updatedValues],
+    };
+  
+    googleSheets.spreadsheets.values.update({
+      spreadsheetId,
+      range,
+      valueInputOption: 'RAW',
+      resource: requestBody,
+    }, (err, response) => {
+      if (err) {
+        console.error('The API returned an error:', err);
+        res.send('Error updating row');
+        return;
+      }
+  
+      res.redirect('/home'); // Redirect to the main page or any other desired location
+    });
+  });
 /*ROUTER PARA METODOS DEL CONTROLLER*/ 
 router.post('/register', authController.register)
 router.post('/login', authController.login)
