02e2825f8b35dfbf1e9f9ff69c81d59ebb40cd52
Mariorl14 <mariorl040301@gmail.com>
2023-06-13 13:40:33 -0600

    vista fijo

--- routes/router.js
+++ routes/router.js
@@ -256,6 +256,46 @@ router.get('/listarVentasGoogle', authController.isAuthenticated, NoCache.nocach
     res.render('listarVentasGoogle', {rows, user:user});
     
 })
+router.get('/listarVentasGoogleFijo', authController.isAuthenticated, NoCache.nocache,async (req, res)=>{
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+/// client instance for auth
+  const client = await auth.getClient();
+
+  /// Instance of google sheets api 
+  const googleSheets = google.sheets({ version: "v4", auth: client});
+
+
+  const spreadsheetId = "1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY";
+  // Get DATA 
+
+
+
+  const metaData = await googleSheets.spreadsheets.get({
+      auth,
+      spreadsheetId
+  });
+
+  /// rows from spreadsheet
+
+ 
+
+  const ventas = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "Registro_Ventas_Fijo!A2:AJ",
+  })
+
+  var user = req.user;
+  const rows = ventas.data.values;
+
+
+  res.render('listarVentasGoogleFijo', {rows, user:user});
+  
+})
 router.get('/listarVentasFijo', authController.isAuthenticated, NoCache.nocache,async (req, res)=>{
     const auth = new google.auth.GoogleAuth({
         keyFile: "credentials.json",
