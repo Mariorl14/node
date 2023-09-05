3f168b0c7c1f0e2f477dfa278b0b0cd006e091d0
Mariorl14 <mariorl040301@gmaiol.com>
2023-09-05 15:08:55 -0600

    Base ITX ready

--- routes/router.js
+++ routes/router.js
@@ -483,6 +483,46 @@ router.get('/bdKolbi',authController.isAuthenticated, NoCache.nocache,authContro
 
     res.render('bdKolbi', {rows, user:user});
 })
+/* base ITX */
+router.get('/bdITX',authController.isAuthenticated, NoCache.nocache,authController.TicocelBDF, authController.FavtelNicaraguaKolbi, async  (req, res)=>{
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+/// client instance for auth
+  const client = await auth.getClient();
+
+  /// Instance of google sheets api 
+  const googleSheets = google.sheets({ version: "v4", auth: client});
+
+
+  const spreadsheetId = "1aLHA7UdBSUrxC9k-cOL5CG650hoP8WesvpktDs-PRAg";
+  // Get DATA 
+
+
+
+  const metaData = await googleSheets.spreadsheets.get({
+      auth,
+      spreadsheetId
+  });
+
+  /// rows from spreadsheet
+
+ 
+
+  const ventas = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "Base Madre!A2:J",
+  })
+
+  var user = req.user;
+  const rows = ventas.data.values;
+
+
+  res.render('bdITX', {rows, user:user});
+})
 router.get('/bdKolbiNicaragua',authController.isAuthenticated, NoCache.nocache,authController.TicocelBDF,  async  (req, res)=>{
   const auth = new google.auth.GoogleAuth({
       keyFile: "credentials.json",
@@ -1388,6 +1428,100 @@ router.get('/edit/:rowId', async  (req, res) => {
       }
   
       res.redirect('/bdKolbi'); // Redirect to the main page or any other desired location
+    });
+  });
+
+  /*eDITAR BASE ITX */
+  router.get('/editITX/:rowId', async  (req, res) => {
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+      });
+      
+      // Create the client instance for authentication
+      const client = await auth.getClient();
+      
+      // Create an instance of the Google Sheets API
+      const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    // Get DATA 
+    const rowId = req.params.rowId;
+  
+    // Step 2: Retrieve row data from Google Sheets
+    // Set up authentication as mentioned in the previous response
+  
+    const spreadsheetId = '1aLHA7UdBSUrxC9k-cOL5CG650hoP8WesvpktDs-PRAg';
+    const range = `Base Madre!A${rowId}:L${rowId}`;
+  
+    googleSheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    }, (err, response) => {
+      if (err) {
+        console.error('The API returned an error:', err);
+        res.send('Error retrieving row data');
+        return;
+      }
+  
+      const rowValues = response.data.values[0];
+  
+      // Step 3: Render the update view with retrieved data
+      res.render('updateViewBDITX', { rowId, rowValues }); // Assumes you have an 'update.ejs' view/template
+    });
+  });
+
+  router.post('/editITX/:rowId', async (req, res) => {
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+      });
+      
+      // Create the client instance for authentication
+      const client = await auth.getClient();
+      
+      // Create an instance of the Google Sheets API
+      const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    const rowId = req.params.rowId;
+    const updatedValues = [
+      req.body.column1,
+      req.body.column2,
+      req.body.column3,
+      req.body.column4,
+      req.body.column5,
+      req.body.column6,
+      req.body.column7,
+      req.body.column8,
+      req.body.column9,
+      req.body.column10,
+      req.body.column11,
+      req.body.column12,
+    ];
+  
+    // Set up authentication as mentioned in the previous response
+  
+    const spreadsheetId = '1aLHA7UdBSUrxC9k-cOL5CG650hoP8WesvpktDs-PRAg';
+    const range = `Base Madre!A${rowId}:L${rowId}`;
+  
+    const requestBody = {
+      values: [updatedValues],
+    };
+  
+    googleSheets.spreadsheets.values.update({
+      spreadsheetId,
+      range,
+      valueInputOption: 'RAW',
+      resource: requestBody,
+    }, (err, response) => {
+      if (err) {
+        console.error('The API returned an error:', err);
+        res.send('Error updating row');
+        return;
+      }
+  
+      res.redirect('/bdITX'); // Redirect to the main page or any other desired location
     });
   });
 
