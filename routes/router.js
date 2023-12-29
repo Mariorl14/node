939022743e0b4cf374181d47d60be73338b28cd2
Mariorl14 <mariorl040301@gmaiol.com>
2023-12-29 04:33:59 -0600

    PostVentas Crteado

--- routes/router.js
+++ routes/router.js
@@ -890,7 +890,8 @@ router.get('/listarVentasGoogle', authController.isAuthenticated, NoCache.nocach
     res.render('listarVentasGoogle', {rows, user:user});
     
 })
-router.get('/listarVentasGoogleFijo', authController.isAuthenticated, NoCache.nocache, authController.TicocelLVFTIC,async (req, res)=>{
+/* POSTVENTA MOVIL */
+router.get('/PostVentaMovil', authController.isAuthenticated, NoCache.nocache, authController.TicocelLVFTIC,async (req, res)=>{
   const auth = new google.auth.GoogleAuth({
       keyFile: "credentials.json",
       scopes: "https://www.googleapis.com/auth/spreadsheets",
@@ -920,14 +921,55 @@ router.get('/listarVentasGoogleFijo', authController.isAuthenticated, NoCache.no
   const ventas = await googleSheets.spreadsheets.values.get({
       auth,
       spreadsheetId,
-      range: "Registro_Ventas_Fijo!A2:AJ",
+      range: "PostVenta Movil!A2:T",
   })
 
   var user = req.user;
   const rows = ventas.data.values;
 
 
-  res.render('listarVentasGoogleFijo', {rows, user:user});
+  res.render('PostVentaMovil', {rows, user:user});
+  
+})
+/* POSTVENTA FIJO */
+router.get('/PostVentaFijo', authController.isAuthenticated, NoCache.nocache, authController.TicocelLVFTIC,async (req, res)=>{
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+/// client instance for auth
+  const client = await auth.getClient();
+
+  /// Instance of google sheets api 
+  const googleSheets = google.sheets({ version: "v4", auth: client});
+
+
+  const spreadsheetId = "1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY";
+  // Get DATA 
+
+
+
+  const metaData = await googleSheets.spreadsheets.get({
+      auth,
+      spreadsheetId
+  });
+
+  /// rows from spreadsheet
+
+ 
+
+  const ventas = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "PostVenta Fijo!A2:T",
+  })
+
+  var user = req.user;
+  const rows = ventas.data.values;
+
+
+  res.render('PostVentaFijo', {rows, user:user});
   
 })
 /* TABLA DE VENTAS FIJOOO DE TICOCEL */
@@ -3006,6 +3048,216 @@ router.post('/editFacturasNoviembre/:rowId', async (req, res) => {
       res.render('editarVentas', { rowId, rowValues }); // Assumes you have an 'update.ejs' view/template
     });
   });
+
+  /* Edit POST VENTAS Movil */
+  router.get('/editPostVentaMovil/:rowId', async  (req, res) => {
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+      });
+      
+      // Create the client instance for authentication
+      const client = await auth.getClient();
+      
+      // Create an instance of the Google Sheets API
+      const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    // Get DATA 
+    const rowId = req.params.rowId;
+  
+    // Step 2: Retrieve row data from Google Sheets
+    // Set up authentication as mentioned in the previous response
+  
+    const spreadsheetId = '1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY';
+    const range = `PostVenta Movil!A${rowId}:T${rowId}`;
+  
+    googleSheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    }, (err, response) => {
+      if (err) {
+        console.error('The API returned an error:', err);
+        res.send('Error retrieving row data');
+        return;
+      }
+  
+      const rowValues = response.data.values[0];
+  
+      // Step 3: Render the update view with retrieved data
+      res.render('editarPostVentasMovil', { rowId, rowValues }); // Assumes you have an 'update.ejs' view/template
+    });
+  });
+
+  router.post('/editPostVentaMovil/:rowId', async (req, res) => {
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+      });
+      
+      // Create the client instance for authentication
+      const client = await auth.getClient();
+      
+      // Create an instance of the Google Sheets API
+      const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    const rowId = req.params.rowId;
+    const updatedValues = [
+      req.body.column1,
+      req.body.column2,
+      req.body.column3,
+      req.body.column4,
+      req.body.column5,
+      req.body.column6,
+      req.body.column7,
+      req.body.column8,
+      req.body.column9,
+      req.body.column10,
+      req.body.column11,
+      req.body.column12,
+      req.body.column13,
+      req.body.column14,
+      req.body.column15,
+      req.body.column16,
+      req.body.column17,
+      req.body.column18,
+      req.body.column19,
+      req.body.column20
+    ];
+  
+    // Set up authentication as mentioned in the previous response
+  
+    const spreadsheetId = '1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY';
+    const range = `PostVenta Movil!A${rowId}:T${rowId}`;
+  
+    const requestBody = {
+      values: [updatedValues],
+    };
+  
+    googleSheets.spreadsheets.values.update({
+      spreadsheetId,
+      range,
+      valueInputOption: 'RAW',
+      resource: requestBody,
+    }, (err, response) => {
+      if (err) {
+        console.error('The API returned an error:', err);
+        res.send('Error updating row');
+        return;
+      }
+  
+      res.redirect('/PostVentaMovil'); // Redirect to the main page or any other desired location
+    });
+  });
+
+
+
+/* Edit POST VENTAS Fijo */
+router.get('/editPostVentaFijo/:rowId', async  (req, res) => {
+
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    
+    // Create the client instance for authentication
+    const client = await auth.getClient();
+    
+    // Create an instance of the Google Sheets API
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  // Get DATA 
+  const rowId = req.params.rowId;
+
+  // Step 2: Retrieve row data from Google Sheets
+  // Set up authentication as mentioned in the previous response
+
+  const spreadsheetId = '1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY';
+  const range = `PostVenta Fijo!A${rowId}:T${rowId}`;
+
+  googleSheets.spreadsheets.values.get({
+    spreadsheetId,
+    range,
+  }, (err, response) => {
+    if (err) {
+      console.error('The API returned an error:', err);
+      res.send('Error retrieving row data');
+      return;
+    }
+
+    const rowValues = response.data.values[0];
+
+    // Step 3: Render the update view with retrieved data
+    res.render('editarPostVentasFijo', { rowId, rowValues }); // Assumes you have an 'update.ejs' view/template
+  });
+});
+
+router.post('/editPostVentaFijo/:rowId', async (req, res) => {
+
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    
+    // Create the client instance for authentication
+    const client = await auth.getClient();
+    
+    // Create an instance of the Google Sheets API
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const rowId = req.params.rowId;
+  const updatedValues = [
+    req.body.column1,
+    req.body.column2,
+    req.body.column3,
+    req.body.column4,
+    req.body.column5,
+    req.body.column6,
+    req.body.column7,
+    req.body.column8,
+    req.body.column9,
+    req.body.column10,
+    req.body.column11,
+    req.body.column12,
+    req.body.column13,
+    req.body.column14,
+    req.body.column15,
+    req.body.column16,
+    req.body.column17,
+    req.body.column18,
+    req.body.column19,
+    req.body.column20
+  ];
+
+  // Set up authentication as mentioned in the previous response
+
+  const spreadsheetId = '1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY';
+  const range = `PostVenta Fijo!A${rowId}:T${rowId}`;
+
+  const requestBody = {
+    values: [updatedValues],
+  };
+
+  googleSheets.spreadsheets.values.update({
+    spreadsheetId,
+    range,
+    valueInputOption: 'RAW',
+    resource: requestBody,
+  }, (err, response) => {
+    if (err) {
+      console.error('The API returned an error:', err);
+      res.send('Error updating row');
+      return;
+    }
+
+    res.redirect('/PostVentaFijo'); // Redirect to the main page or any other desired location
+  });
+});
+
+
+
+
  router.post('/editVentasGoogle/:rowId', async (req, res) => {
 
     const auth = new google.auth.GoogleAuth({
