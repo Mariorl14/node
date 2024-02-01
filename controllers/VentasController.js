1b311e426a0eb00da0cb85cf27ae2a3eabbfbbbe
Mariorl14 <mariorl040301@gmail.com>
2024-02-01 13:24:59 -0600

    FEBRERO

--- controllers/VentasController.js
+++ controllers/VentasController.js
@@ -228,7 +228,7 @@ exports.registrarVentaGoogle = async (req, res) => {
     var NumeroActivacion = "";
     var FechaE = "";
     var Bloqueo = "Bloqueada";
-    var MesTrabajada = "ENERO";
+    var MesTrabajada = "FEBRERO";
     var Activadora = "";
     var Comision = "";
 
@@ -612,7 +612,7 @@ exports.registrarVentaFijo = async (req, res) => {
     var LlamadaACT = "";
     var FechaACT = "";
     var NumeroOrden = "";
-    var MesTrabajada = "ENERO";
+    var MesTrabajada = "FEBRERO";
     var InstalacionPrograda = "";
     var Comision = "";
 
