3f79d5526d63a7f566c46516b19ec6cfbbb974ef
Mariorl14 <mariorl040301@gmail.com>
2024-03-06 23:34:52 -0600

    revenue febrero

--- routes/router.js
+++ routes/router.js
@@ -611,6 +611,61 @@ router.get('/revenueEnero2024', authController.isAuthenticated,authController.au
   
 
 })
+router.get('/revenueFebrero2024', authController.isAuthenticated,authController.authRol, NoCache.nocache,  async (req, res)=>{
+    
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+/// client instance for auth
+  const client = await auth.getClient();
+
+  /// Instance of google sheets api 
+  const googleSheets = google.sheets({ version: "v4", auth: client});
+
+
+  const spreadsheetId = "1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY";
+  // Get DATA 
+
+
+
+  const metaData = await googleSheets.spreadsheets.get({
+      auth,
+      spreadsheetId
+  });
+
+  /// rows from spreadsheet
+
+ 
+
+  const ventas = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "Revenue Febrero 2024!A2:AB",
+  })
+
+  var user = req.user;
+  const rows = ventas.data.values;
+
+  if(rows==undefined){
+      rows="";
+  }
+  let query1 = 'SELECT * FROM users';
+  let results = {};
+  
+  conexion.query(query1, (error, data) => {
+      if (error) {
+          throw error;
+      } else {
+          results.users = data;
+          // check if the second query has also been executed
+              res.render('revenueFebrero2024', {rows:rows, data: results, user: req.user });
+      }
+  });
+  
+
+})
 router.get('/register', authController.isAuthenticated,authController.authRol, NoCache.nocache,  async (req, res)=>{
     res.render('register', {user:req.user})
 })
@@ -3375,6 +3430,116 @@ router.post('/editFacturasEnero/:rowId', async (req, res) => {
     res.redirect('/revenueEnero2024'); // Redirect to the main page or any other desired location
   });
 }); 
+
+/*EDITAR revenue Febrero 2024*/
+router.get('/editFacturasFebrero2024/:rowId', async  (req, res) => {
+
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    
+    // Create the client instance for authentication
+    const client = await auth.getClient();
+    
+    // Create an instance of the Google Sheets API
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  // Get DATA 
+  const rowId = req.params.rowId;
+
+  // Step 2: Retrieve row data from Google Sheets
+  // Set up authentication as mentioned in the previous response
+
+  const spreadsheetId = '1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY';
+  const range = `Revenue Febrero 2024!A${rowId}:AB${rowId}`;
+
+  googleSheets.spreadsheets.values.get({
+    spreadsheetId,
+    range,
+  }, (err, response) => {
+    if (err) {
+      console.error('The API returned an error:', err);
+      res.send('Error retrieving row data');
+      return;
+    }
+
+    const rowValues = response.data.values[0];
+
+    // Step 3: Render the update view with retrieved data
+    res.render('editarRevenueFebrero2024', { rowId, rowValues }); // Assumes you have an 'update.ejs' view/template
+  });
+});
+router.post('/editFacturasFebrero2024/:rowId', async (req, res) => {
+
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    
+    // Create the client instance for authentication
+    const client = await auth.getClient();
+    
+    // Create an instance of the Google Sheets API
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const rowId = req.params.rowId;
+  const updatedValues = [
+    req.body.column1,
+    req.body.column2,
+    req.body.column3,
+    req.body.column4,
+    req.body.column5,
+    req.body.column6,
+    req.body.column7,
+    req.body.column8,
+    req.body.column9,
+    req.body.column10,
+    req.body.column11,
+    req.body.column12,
+    req.body.column13,
+    req.body.column14,
+    req.body.column15,
+    req.body.column16,
+    req.body.column17,
+    req.body.column18,
+    req.body.column19,
+    req.body.column20,
+    req.body.column21,
+    req.body.column22,
+    req.body.column23,
+    req.body.column24,
+    req.body.column25,
+    req.body.column26,
+    req.body.column27,
+    req.body.column28
+  ];
+
+  // Set up authentication as mentioned in the previous response
+
+  const spreadsheetId = '1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY';
+  const range = `Revenue Febrero 2024!A${rowId}:AB${rowId}`;
+
+  const requestBody = {
+    values: [updatedValues],
+  };
+
+  googleSheets.spreadsheets.values.update({
+    spreadsheetId,
+    range,
+    valueInputOption: 'RAW',
+    resource: requestBody,
+  }, (err, response) => {
+    if (err) {
+      console.error('The API returned an error:', err);
+      res.send('Error updating row');
+      return;
+    }
+
+    res.redirect('/revenueFebrero2024'); // Redirect to the main page or any other desired location
+  });
+}); 
+
 
   router.get('/editVentas/:rowId', async  (req, res) => {
 
