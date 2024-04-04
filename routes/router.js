dfa43433cd3e691369dd3de08dad1c1f6657b037
Mariorl14 <mariorl040301@gmail.com>
2024-04-03 21:23:15 -0600

    Bases Nuevas

--- routes/router.js
+++ routes/router.js
@@ -981,6 +981,91 @@ router.get('/bdKolbi',authController.isAuthenticated, NoCache.nocache,authContro
 
     res.render('bdKolbi', {rows, user:user});
 })
+
+/* BASE MIGRACIONES */
+
+router.get('/bdMigraciones',authController.isAuthenticated, NoCache.nocache,authController.TicocelBDF, authController.FavtelNicaraguaKolbi, async  (req, res)=>{
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+/// client instance for auth
+  const client = await auth.getClient();
+
+  /// Instance of google sheets api 
+  const googleSheets = google.sheets({ version: "v4", auth: client});
+
+
+  const spreadsheetId = "16q_ic76h568Y9XlTKipMA_HlPWcnt-cQZmgJ7tau4VI";
+  // Get DATA 
+
+
+
+  const metaData = await googleSheets.spreadsheets.get({
+      auth,
+      spreadsheetId
+  });
+
+  /// rows from spreadsheet
+
+ 
+
+  const ventas = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "Base Madre!A2:I",
+  })
+
+  var user = req.user;
+  const rows = ventas.data.values;
+
+
+  res.render('bdMigraciones', {rows, user:user});
+})
+
+/* BASE TELEFONOS */
+
+router.get('/bdTelefonos',authController.isAuthenticated, NoCache.nocache,authController.TicocelBDF, authController.FavtelNicaraguaKolbi, async  (req, res)=>{
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+/// client instance for auth
+  const client = await auth.getClient();
+
+  /// Instance of google sheets api 
+  const googleSheets = google.sheets({ version: "v4", auth: client});
+
+
+  const spreadsheetId = "1ewvh5iI-xF9n6j1VNYdeYwY8DrJW-j7z-XV2J3RpdNc";
+  // Get DATA 
+
+
+
+  const metaData = await googleSheets.spreadsheets.get({
+      auth,
+      spreadsheetId
+  });
+
+  /// rows from spreadsheet
+
+ 
+
+  const ventas = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "Base Madre!A2:I",
+  })
+
+  var user = req.user;
+  const rows = ventas.data.values;
+
+
+  res.render('bdTelefonos', {rows, user:user});
+})
+
 /* base ITX */
 router.get('/bdITX',authController.isAuthenticated, NoCache.nocache,authController.TicocelBDF, authController.FavtelNicaraguaKolbi, async  (req, res)=>{
   const auth = new google.auth.GoogleAuth({
@@ -1997,6 +2082,100 @@ router.get('/edit/:rowId', async  (req, res) => {
       }
   
       res.redirect('/bdKolbi'); // Redirect to the main page or any other desired location
+    });
+  });
+
+  /*eDITAR BASE Telefonos */
+  router.get('/editBdTelefonos/:rowId', async  (req, res) => {
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+      });
+      
+      // Create the client instance for authentication
+      const client = await auth.getClient();
+      
+      // Create an instance of the Google Sheets API
+      const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    // Get DATA 
+    const rowId = req.params.rowId;
+  
+    // Step 2: Retrieve row data from Google Sheets
+    // Set up authentication as mentioned in the previous response
+  
+    const spreadsheetId = '1ewvh5iI-xF9n6j1VNYdeYwY8DrJW-j7z-XV2J3RpdNc';
+    const range = `Base Madre!A${rowId}:L${rowId}`;
+  
+    googleSheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    }, (err, response) => {
+      if (err) {
+        console.error('The API returned an error:', err);
+        res.send('Error retrieving row data');
+        return;
+      }
+  
+      const rowValues = response.data.values[0];
+  
+      // Step 3: Render the update view with retrieved data
+      res.render('updateViewBDTelefonos', { rowId, rowValues }); // Assumes you have an 'update.ejs' view/template
+    });
+  });
+
+  router.post('/editBdTelefonos/:rowId', async (req, res) => {
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+      });
+      
+      // Create the client instance for authentication
+      const client = await auth.getClient();
+      
+      // Create an instance of the Google Sheets API
+      const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    const rowId = req.params.rowId;
+    const updatedValues = [
+      req.body.column1,
+      req.body.column2,
+      req.body.column3,
+      req.body.column4,
+      req.body.column5,
+      req.body.column6,
+      req.body.column7,
+      req.body.column8,
+      req.body.column9,
+      req.body.column10,
+      req.body.column11,
+      req.body.column12,
+    ];
+  
+    // Set up authentication as mentioned in the previous response
+  
+    const spreadsheetId = '1ewvh5iI-xF9n6j1VNYdeYwY8DrJW-j7z-XV2J3RpdNc';
+    const range = `Base Madre!A${rowId}:L${rowId}`;
+  
+    const requestBody = {
+      values: [updatedValues],
+    };
+  
+    googleSheets.spreadsheets.values.update({
+      spreadsheetId,
+      range,
+      valueInputOption: 'RAW',
+      resource: requestBody,
+    }, (err, response) => {
+      if (err) {
+        console.error('The API returned an error:', err);
+        res.send('Error updating row');
+        return;
+      }
+  
+      res.redirect('/bdTelefonos'); // Redirect to the main page or any other desired location
     });
   });
 
