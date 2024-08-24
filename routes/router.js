de2eea0c202d5a4345a9242fb33522d4290d76e2
Mariorl14 <mariorl040301@gmaiol.com>
2024-08-23 18:41:58 -0600

    Error bonito

--- routes/router.js
+++ routes/router.js
@@ -2688,45 +2688,44 @@ router.get('/edit/:rowId', async  (req, res) => {
   });
 
 /*EDITAR HOMEE */
-  router.get('/editHOME/:rowId', async  (req, res) => {
+router.get('/editHOME/:rowId', async  (req, res) => {
 
 
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-      });
-      
-      // Create the client instance for authentication
-      const client = await auth.getClient();
-      
-      // Create an instance of the Google Sheets API
-      const googleSheets = google.sheets({ version: "v4", auth: client });
-
-    // Get DATA 
-    const rowId = req.params.rowId;
-  
-    // Step 2: Retrieve row data from Google Sheets
-    // Set up authentication as mentioned in the previous response
-  
-    const spreadsheetId = '1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY';
-    const range = `Respuestas_Formulario!A${rowId}:AR${rowId}`;
-  
-    googleSheets.spreadsheets.values.get({
-      spreadsheetId,
-      range,
-    }, (err, response) => {
-      if (err) {
-        console.error('The API returned an error:', err);
-        res.send('Error retrieving row data');
-        return;
-      }
-  
-      const rowValues = response.data.values[0];
-  
-      // Step 3: Render the update view with retrieved data
-      res.render('editarHOME', { rowId, rowValues}); // Assumes you have an 'update.ejs' view/template
+  const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
+    
+    // Create the client instance for authentication
+    const client = await auth.getClient();
+    
+    // Create an instance of the Google Sheets API
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+  // Get DATA 
+  const rowId = req.params.rowId;
+
+  // Step 2: Retrieve row data from Google Sheets
+  // Set up authentication as mentioned in the previous response
+
+  const spreadsheetId = '1vhWdDiGNYWnQp9WbHzZflejPzMM-5g08UGmvNu8B5SY';
+  const range = `Respuestas_Formulario!A${rowId}:AR${rowId}`;
+
+  googleSheets.spreadsheets.values.get({
+    spreadsheetId,
+    range,
+  }, (err, response) => {
+    if (err) {
+      console.error('The API returned an error:', err);
+      res.send('Error retrieving row data');
+      return;
+    }
+
+    const rowValues = response.data.values[0];
+
+    // Step 3: Render the update view with retrieved data
+    res.render('editarHOME', { rowId, rowValues }); // Assumes you have an 'update.ejs' view/template
   });
+});
  router.post('/editHOME/:rowId', async (req, res) => {
 
     const auth = new google.auth.GoogleAuth({
