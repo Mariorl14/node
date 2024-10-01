b875c6c41c2890928a80ec902214d53f862f8a65
Mariorl14 <mariorl040301@gmail.com>
2024-10-01 11:51:28 -0600

    CAMBIO MES TRABAJADA OCTUBRE

--- controllers/VentasController.js
+++ controllers/VentasController.js
@@ -90,7 +90,7 @@ exports.registrarVenta = async (req, res)=>{
     var Fecha_Ultima_Actualizacion = "";
     var Bloqueo_Desbloqueo = "Bloqueada";
     var Activadora = "";
-    var MES_TRABAJADA = "SETIEMBRE";
+    var MES_TRABAJADA = "OCTUBRE";
     const Terminal = req.body.terminal;
     const correo = req.body.correo;
     const Genero = req.body.genero;
@@ -291,7 +291,7 @@ exports.registrarVentaGoogle = async (req, res) => {
     var NumeroActivacion = "";
     var FechaE = "";
     var Bloqueo = "Bloqueada";
-    var MesTrabajada = "SETIEMBRE";
+    var MesTrabajada = "OCTUBRE";
     var Activadora = "";
     var Comision = "";
     var NumeroProvisional = "";
@@ -536,7 +536,7 @@ exports.registrarVentaFijo = async (req, res) => {
     var Llamada_Activacion = "";
     var Fecha_Activacion = "";
     var Numero_Orden = "";
-    var MES_TRABAJADA = "SETIEMBRE";
+    var MES_TRABAJADA = "OCTUBRE";
     var Fecha_Instalacion = "";
     var Fecha_Ultima_Actualizacion = "";
     var Pago_Comision = "";
