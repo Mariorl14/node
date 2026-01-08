d349ef8317ae08483b7b2dd5f4a5a9bea0c9de83
Mariorl14 <mariorl040301@gmail.com>
2026-01-07 23:27:25 -0600

    ener 2026

--- controllers/VentasController.js
+++ controllers/VentasController.js
@@ -113,7 +113,7 @@ exports.registrarVenta = async (req, res)=>{
     var Fecha_Ultima_Actualizacion = "";
     var Bloqueo_Desbloqueo = "Bloqueada";
     var Activadora = "";
-    var MES_TRABAJADA = "NOVIEMBRE 2025";
+    var MES_TRABAJADA = "ENERO 2026";
     const Terminal = req.body.terminal;
     const correo = req.body.correo;
     const Genero = req.body.genero;
@@ -277,7 +277,7 @@ exports.registrarVentaFijo = async (req, res) => {
     const Llamada_Activacion = "";
     const Fecha_Activacion = "";
     const Numero_Orden = "";
-    const MES_TRABAJADA = "NOVIEMBRE 2025";
+    const MES_TRABAJADA = "ENERO 2026";
     const Fecha_Instalacion = "";
     const Fecha_Ultima_Actualizacion = "";
     const Pago_Comision = "";
